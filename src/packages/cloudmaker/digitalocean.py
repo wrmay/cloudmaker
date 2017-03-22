c3da3152232c02a36c4774384af5bae96da92506
Randy May <randy@mathysphere.com>
2017-03-22 04:44:50 -0400

    look for security file in ~/.cloudmaker.json, not current dir

--- src/packages/cloudmaker/digitalocean.py
+++ src/packages/cloudmaker/digitalocean.py
@@ -5,28 +5,30 @@ import json
 import os.path
 import sys
 
-SECURITY_FILE='security.json'
+SECURITY_FILE='~/.cloudmaker.json'
 DO_API_HOST='api.digitalocean.com'
 PER_PAGE=1000
 DIGITAL_OCEAN_API_KEY='digital_ocean_api_key'
 PUBLIC_SSH_KEY='public_ssh_key'
 
 class Context:
-    
+
     def __init__(self):
-        if not os.path.isfile(SECURITY_FILE):
-            raise Exception('could not initialize Digital Ocean Context because the security file, ' + SECURITY_FILE + 'does not exist' )
-        
-        with open(SECURITY_FILE,'r') as securityFile:
-            self.securityInfo = json.load(securityFile)
-            print('loaded security information from ' + os.path.abspath(SECURITY_FILE), file=sys.stderr)
-            
+        securityFile = os.path.expanduser(SECURITY_FILE)
+
+        if not os.path.isfile(securityFile):
+            raise Exception('could not initialize Digital Ocean Context because the security file, ' + securityFile + 'does not exist' )
+
+        with open(securityFile,'r') as sFile:
+            self.securityInfo = json.load(sFile)
+            print('loaded security information from ' + securityFile, file=sys.stderr)
+
         if not DIGITAL_OCEAN_API_KEY in self.securityInfo:
             raise Exception(DIGITAL_OCEAN_API_KEY + ' not found in security file')
-        
+
         if not PUBLIC_SSH_KEY in self.securityInfo:
             raise Exception(PUBLIC_SSH_KEY + ' not found in security file')
-        
+
         listKeysResponse = self.doGET('/v2/account/keys')
         self.sshKeyId = None
         for key in listKeysResponse['ssh_keys']:
@@ -34,12 +36,12 @@ class Context:
                 print('ssh key already registered', file=sys.stderr)
                 self.sshKeyId = key['id']
                 break
-        
+
         if self.sshKeyId is None:
             resp = self.registerSSHKey(self.securityInfo[PUBLIC_SSH_KEY])
             self.sshKeyId = resp['ssh_key']['id']
             print('ssh key registered', file = sys.stderr)
-        
+
     def doPOST(self, path, body):
         conn = httplib.HTTPSConnection(DO_API_HOST)
         headers = dict()
@@ -50,10 +52,10 @@ class Context:
         resp = conn.getresponse()
         if resp.status != 201 and resp.status != 202:
             raise Exception('an error occurred while invoking POST on {2}  - http response was {0}/{1}'.format(resp.status,resp.reason, path))
-    
+
         responseJSON = json.load(resp)
         return responseJSON
-            
+
     def doGET(self, path):
         conn = httplib.HTTPSConnection(DO_API_HOST)
         headers = dict()
@@ -63,7 +65,7 @@ class Context:
         resp = conn.getresponse()
         if resp.status != 200:
             raise Exception('an error occurred while invoking GET on {2}  - http response was {0}/{1}'.format(resp.status,resp.reason, path))
-    
+
         responseJSON = json.load(resp)
         return responseJSON
 
@@ -79,38 +81,38 @@ class Context:
 
     def listImages(self):
         return self.doGET('/v2/images')
-    
+
     def listSizes(self):
         return self.doGET('/v2/sizes')
-    
+
     def listRegions(self):
         return self.doGET('/v2/regions')
-    
+
     def listDroplets(self):
         return self.doGET('/v2/droplets')
-    
+
     def listDomains(self):
         return self.doGET('/v2/domains')
-    
+
     def listDomainRecords(self, domainName):
         return self.doGET('/v2/domains/' + domainName + '/records')
-    
+
     def getAction(self, id):
         return self.doGET('/v2/actions/{0}'.format(id))
-    
+
     def getDroplet(self, id):
         return self.doGET('/v2/droplets/{0}'.format(id))
-    
+
     def registerSSHKey(self, key):
         body = dict()
         body['public_key'] = key
         return self.doPOST('/v2/account/keys', body)
-    
+
     def createDroplet(self, request):
         # seems like this class should not be doing this - consider refactoring
         request['ssh_keys'] = [self.sshKeyId]
         return self.doPOST('/v2/droplets', request)
-            
+
     def createDomain(self, domain, ipAddress):
         body = dict()
         body['name'] = domain
@@ -127,28 +129,28 @@ class Context:
     def deleteDomain(self, domainName):
         self.doDELETE('/v2/domains/' + domainName)
 
-    
+
     def deleteDomainRecord(self, domainName, recordId):
         self.doDELETE('/v2/domains/' + domainName + '/records/{0}'.format(recordId))
-        
+
     def deleteDroplet(self, dropletId):
         self.doDELETE('/v2/droplets/{0}'.format(dropletId) )
-        
-            
+
+
     def publicAddressIPV4(self, droplet):
         for network in droplet['networks']['v4']:
             if network['type'] == 'public':
                 return network['ip_address']
-        
+
         return None
-    
+
     def publicAddressIPV6(self, droplet):
         for network in droplet['networks']['v6']:
             if network['type'] == 'public':
                 return network['ip_address']
-        
+
         return None
-    
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == 'list':
@@ -167,8 +169,6 @@ if __name__ == '__main__':
                 sizes = do.listSizes()
                 json.dump(sizes,sys.stdout,indent=3)
                 sys.exit(0)
-        
+
     print('usage', file=sys.stderr)
     print('\tdigitalocean list regions        #list all regions', file=sys.stderr)
-              
-    
